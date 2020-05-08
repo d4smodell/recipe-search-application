4fa9a59af293bce9a89b56347ad2763090ae1316
Daniil <daniilrealgrad@gmail.com>
2020-05-08 10:54:46 +0300

    Project is done

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import RecipeDetails from './RecipeDetails'
 
-const Recipe = () => {
+const Recipe = ({recipe}) => {
+    const [show, setShow] = useState(false)
+    const {label, image, url, ingredients} = recipe.recipe
     return (
-        <div>Recipe</div>
+        <div className='recipe'>
+            <h2>{label}</h2>
+            <img src={image} alt={label}/>
+            <a href={url} target='blacnk' rel='noopener noreferrer'>URL</a>
+            <button onClick={() => setShow(!show)}>Ingredients</button>
+            {show ? <RecipeDetails ingredients={ingredients}/> : null}
+        </div>
     )
 }
 

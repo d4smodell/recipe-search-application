817785a41d98d2e2504ad0df49a33a9a3689f8c9
Daniil <daniilrealgrad@gmail.com>
2020-05-08 10:29:07 +0300

    half of app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
-import React from 'react'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
+import Axios from "axios";
+import Recipe from './components/Recipe'
 
 const App = () => {
-    return (
-        <div>
-            <h1>Food Searching App</h1>
-        </div>
-    )
-}
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([])
 
-export default App
+  const APP_ID = "8c74d70f";
+  const APP_KEY = "4d133fe698cf66f9a457d86ca21cc42e";
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`;
+
+  const getData = async () => {
+    const result = await Axios.get(url);
+    setRecipes(result.data.hits)
+
+    console.log(result);
+    setQuery('')
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    getData();
+  };
+
+  const onChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  return (
+    <div className="App">
+      <h1 onClick={getData}>Food Searching App</h1>
+      <form className="search-form" onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Search Food"
+          autoComplete="off"
+          onChange={onChange}
+          value={query}
+        />
+        <input type="submit" value="search" />
+      </form>
+      <div className='recipes'>
+        {recipes !== [] && recipes.map(recipe => <Recipe recipe={recipe} key={recipe.id}/>)}
+      </div>
+    </div>
+  );
+};
+
+export default App;
